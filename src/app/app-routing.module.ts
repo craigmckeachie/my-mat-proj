b82653e125081e64f7f179a2349ccdcdbae235f8
Craig McKeachie <craigdmckeachie@gmail.com>
2021-09-07 15:59:51 -0400

    exercise

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddressFormComponent } from './address-form/address-form.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MyTableComponent } from './my-table/my-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', pathMatch: 'full', redirectTo: '' },
+  { path: '', component: DashboardComponent },
+  { path: 'contact', component: AddressFormComponent },
+  { path: 'table', component: MyTableComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
